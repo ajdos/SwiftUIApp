e20de0f67703e881933cbcb5a5cc48797136e7f2
Aidin <aidin.abdurakhmanov@gmail.com>
2020-11-08 18:18:16 +0300

    Initial Commit

---
+++ SwiftUIApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIApp
+//
+//  Created by Айдин Абдурахманов on 08.11.2020.
+//  Copyright © 2020 Айдин Абдурахманов. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
